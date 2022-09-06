8ff588e25db4a0e65f6542f0fee6e94d951002b8
NikitaStambul <stambul.n@gmail.com>
2022-09-06 14:17:48 +0300

    Initial Commit

---
+++ ExitekDeveloperTechTask/ExitekDeveloperTechTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExitekDeveloperTechTaskApp.swift
+//  ExitekDeveloperTechTask
+//
+//  Created by Nikita Stambul on 06.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExitekDeveloperTechTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
